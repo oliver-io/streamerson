3dd0cffdda01a4192268122c4b2c1870d5250439
oliver-io <oliver.l.carrillo@gmail.com>
2023-10-25 21:38:53 -0500

    v0.0.2

--- packages/benchmarking/tools/start-benchmark.sh
+++ packages/benchmarking/tools/start-benchmark.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-RUNNER=$1
-TARGET=$2
-SCRIPT=$3
+RUNNER="${1:-node}"
+TARGET="${2:-$STREAMERSON_BENCHMARK_DIRECTORY}"
+SCRIPT="${3:-$STREAMERSON_BENCHMARK_SCRIPT}"
+echo "Running benchmark with $RUNNER for target: $TARGET ($SCRIPT)"
 
-echo "Running benchmark with $RUNNER for target: $TARGET"
 ## find a directory with the name of the target:
 DIR=$(find . -type d -name $TARGET)
 
--- tools/run-benchmark.ts
+++ tools/run-benchmark.ts
@@ -3,34 +3,65 @@ import minimist from 'minimist';
 import path from 'path';
 import {execSync} from 'child_process';
 
-const args = minimist(process.argv.slice(2), {});
-
-if (!args.target) {
-  throw new Error("No target specified");
+type CLIOptions = {
+  report?: boolean,
+  build?: boolean,
+  exec?: boolean,
+  target: string
 }
 
-async function run() {
+async function runAllBenchmarksAndMaybeCollectResults(options: CLIOptions) {
+  const appFiles: Array<string> = await glob(`./packages/benchmarking/src/**/*benchmark.ts`);
+  if (!appFiles.length) {
+    throw new Error("No such directory found");
+  }
+
+  for (const _appFile of appFiles) {
+    const appFile = path.posix.resolve(_appFile);
+    const dirname = path.basename(path.dirname(appFile));
+    if (options.target && !options.target.includes(dirname)) {
+      continue;
+    }
+
+    execSync(`yarn benchmark --build --target=${dirname}`, {stdio: 'inherit'});
+    try {
+      await runSingleBenchmark({
+        ...options,
+        target: dirname
+      });
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  execSync('yarn build:summary', {
+    cwd: path.resolve('./packages/benchmarking'), stdio: 'inherit'
+  });
+}
+
+async function runSingleBenchmark(options: CLIOptions) {
   try {
-    const buildReport = args.report ?? true;
+    const buildReport = options.report ?? true;
     const appFiles: Array<string> = await glob(`./packages/benchmarking/src/**/*benchmark.ts`);
     if (!appFiles.length) {
       throw new Error("No such directory found");
     }
 
-    const exec = args.exec ?? false;
+    const exec = options.exec ?? false;
 
-    if (!exec && !args.build) {
+    if (!exec && !options.build) {
       throw new Error("No action specified; must be one or both of --build or --exec");
     }
 
     for (const _appFile of appFiles) {
       const appFile = path.posix.resolve(_appFile);
       const directory = path.basename(path.dirname(appFile));
-      if (args.target && !args.target.includes(directory)) {
+      if (options.target && !options.target.includes(directory)) {
         continue;
       }
 
-      const STREAMERSON_IMAGE_TARGET = `streamerson/benchmarking/${directory}:latest`;
+      const
+        STREAMERSON_IMAGE_TARGET = `streamerson/benchmarking/${directory}:latest`;
       const STREAMERSON_BENCHMARK_APP_TARGET = directory;
       const env = {
         ...process.env,
@@ -39,20 +70,13 @@ async function run() {
         STREAMERSON_PROJECT: directory
       };
 
-      const buildBaseImageCmd = "docker build -t streamerson/benchmarking:latest . -f ./packages/benchmarking/build/benchmarking.dockerfile";
-      const buildDefaultImageCmd = `docker build --build-arg TARGET=${
-        directory
-      } -t streamerson/benchmarking/${
-        directory
-      }:latest . -f ./packages/benchmarking/build/app.dockerfile`;
       // if the directory has its own app.yaml, then we need to use that instead of the default
       const [projectDockerFile] = await glob(`./packages/benchmarking/src/${directory}/app.dockerfile`);
-      const [projectComposeFile] = await glob(`./packages/benchmarking/src/${directory}/app.dockerfile`);
+      const [projectComposeFile] = await glob(`./packages/benchmarking/src/${directory}/app.yaml`);
 
-
-      if (args.build) {
+      if (options.build) {
         const buildCommands = [
-          ...(args.build ? [
+          ...(options.build ? [
             // The base image, which is just the `benchmarking` monorepo package with its dependencies already built:
             `docker build -t streamerson/benchmarking:latest . -f ./packages/benchmarking/build/benchmarking.dockerfile`,
             // The target image, i.e. the benchmarking app
@@ -67,7 +91,7 @@ async function run() {
         }
       }
 
-      if (args.exec) {
+      if (options.exec) {
         const executeCommands = [
           `echo "Starting benchmark for ${directory}"`,
           `docker compose -p ${directory} -f ./build/stack.yaml -f ${projectComposeFile ?? './build/app.yaml'} up --abort-on-container-exit --force-recreate --renew-anon-volumes`,
@@ -85,6 +109,21 @@ async function run() {
     }
   } catch (err) {
     console.error(err);
+  }
+}
+
+async function run() {
+  const args = minimist(process.argv.slice(2), {});
+  if (!args.target) {
+    await runAllBenchmarksAndMaybeCollectResults({
+      ...args,
+      target: "*"
+    });
+  } else {
+    await runSingleBenchmark({
+      ...args,
+      target: args.target
+    });
   }
 }
 
